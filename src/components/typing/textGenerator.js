ad9404e174ad14dde0800279e061146968d1ae15
BeruzDev <135733187+BeruzDev@users.noreply.github.com>
2024-11-16 18:16:25 +0100

    Auto Generated Text done!

--- src/components/typing/textGenerator.js
+++ src/components/typing/textGenerator.js
@@ -3,14 +3,17 @@ import { Random } from 'random-js'
 const random = new Random()
 
 const levels = {
-    level1: 'abcdefghijkl',
+    level1: 'abcdef',
     level2: 'abcdefghijklmnopqrstuvwxyz',
-    level3: 'abcdefghijklmnopqrstuvwxyzñáéíóú',
-    level4: 'abcdefghijklmnopqrstuvwxyzñáéíóú0123456789',
-    level5: 'abcdefghijklmnopqrstuvwxyzñáéíóú0123456789.,!?@#$*&()-=+',
-    level6: `abcdefghijklmnopqrstuvwxyzñáéíóú0123456789.,!?@#$*&()-=+<>/[]{}\\|"'`,
-    level7: 'Enhorabuena, has superado todos los niveles!' //Sigue practicando para mejorar tu mecanografía!
+    level3: 'abcdefghijklmnopqrstuvwxyzñ',
+    level4: 'abcdefghijklmnopqrstuvwxyzñ0123456789',
+    level5: 'abcdefghijklmnopqrstuvwxyzñ0123456789',
+    level6: 'abcdefghijklmnopqrstuvwxyzñ0123456789',
+    level7: 'Enhorabuena, has superado todos los niveles! Sigue practicando para mejorar tu mecanografía!'
 }
+
+const symbols = '.,!?@#$*&()-=+<>/[]{}\\|\'"'
+const accent = 'áéíóú'
 
 // Generar palabras random
 function generateRandomWord(level, minLen = 2, maxLen = 8) {
@@ -18,11 +21,19 @@ function generateRandomWord(level, minLen = 2, maxLen = 8) {
     const wordLength = random.integer(minLen, maxLen) //<-Length de la palabra aleatorio entre minLen y maxLen
     let word = ''
 
+    // Incluir símbolos en los niveles 5 y 6
     for (let i = 0; i < wordLength; i++) {
-        word += random.pick(characters)
+        // Añadir un símbolo con cierta probabilidad dependiendo del nivel
+        if ((level === 'level5' && random.bool(0.1)) || (level === 'level6' && random.bool(0.2))) {
+            word += random.pick(symbols); // Añadir símbolos
+        } else if ((level === 'level3' && random.bool(0.05)) || (level === 'level4' && random.bool(0.1)) || (level === 'level5' && random.bool(0.15)) || (level === 'level6' && random.bool(0.2))) {
+            word += random.pick(accent)// Añadir acentos
+        } else {
+            word += random.pick(characters); // Añadir un carácter del nivel
+        }
     }
 
-    if (level !== 'level1' && random.bool(0.2)) {
+    if (level !== 'level1' && random.bool(0.3)) {
         word = word.charAt(0).toUpperCase() + word.slice(1)
     }
 
@@ -39,7 +50,7 @@ function generateRandomSentences(level, wordCount) {
     }
 
     let punctuation = ''
-    if (level !== 'level1' && level !== 'level2') {
+    if (level !== 'level1' && level !== 'level2' && level  !== 'level3') {
         punctuation = random.pick(['.', '!', '?'])
     } 
     return sentence.join(' ') + punctuation
